217e30027bfcf359daa6708f505d46f3a5022f4a
Yugoun <91927386+Yugoun@users.noreply.github.com>
2022-03-14 23:01:38 +0800

    repair media

--- django_myblog/urls.py
+++ django_myblog/urls.py
@@ -15,13 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.views.static import serve
 from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('article/', include('article.urls', namespace='article')),
     path('userprofile/', include('userprofile.urls', namespace='userprofile')),
     path('password-reset',include('password_reset.urls')),
-    path(r'media/(?P<path>.*)/$', serve, {'document_root': settings.MEDIA_ROOT}),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
